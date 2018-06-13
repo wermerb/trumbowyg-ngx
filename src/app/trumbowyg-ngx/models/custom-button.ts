100879ab291a56377ad8455905d11f0b71a878d8
Balázs Wermer <wermerb@gmail.com>
2018-06-13 19:36:30 +0200

    Update config

--- src/app/trumbowyg-ngx/models/custom-button.ts
+++ src/app/trumbowyg-ngx/models/custom-button.ts
@@ -1,11 +1,11 @@
 export interface CustomButton {
-    fn: string | Function;
-    tag: string;
-    title: string;
-    isSupported: () => boolean;
-    key: string;
-    param: string;
-    forceCSS: boolean;
-    class: string;
-    hasIcon: boolean;
+    fn?: string | Function;
+    tag?: string;
+    title?: string;
+    isSupported?: () => boolean;
+    key?: string;
+    param?: string;
+    forceCSS?: boolean;
+    class?: string;
+    hasIcon?: boolean;
 }
